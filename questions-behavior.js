d4943087305d1a6692fbd05999a9ba7a2acaf41b
Allen Henderson <allenhenderson20@gmail.com>
2021-02-07 17:34:40 -0500

    A little sneak peek of what I've been up to, virtually that is

---
+++ questions-behavior.js
@@ -0,0 +1,57 @@
+var questionsRight = 0;
+var questionsWrong = 0;
+// All questions //
+
+function questionOne() {
+
+    var question1 = prompt("What is your answer?");
+
+    if (question1.toLowerCase() == "george washington")
+
+    {
+
+        document.getElementById("answer1").style.color = "green";
+        document.getElementById("answer1").innerHTML = question1;
+
+    } else  {
+
+         document.getElementById("answer1").style.color = "red";
+         document.getElementById("answer1").innerHTML = question1;
+    }
+}
+
+function questionTwo() {
+
+    var question2 = prompt("What is your answer?");
+
+    if (question2.toLowerCase() == "donald trump")
+
+    {
+
+        document.getElementById("answer2").style.color = "green";
+        document.getElementById("answer2").innerHTML = question2;
+
+    } else  {
+
+         document.getElementById("answer2").style.color = "red";
+         document.getElementById("answer2").innerHTML = question2;
+    }
+}
+// var question3 = prompt("What is your answer?");
+// var question4 = prompt("What is your answer?");
+// var question5 = prompt("What is your answer?");
+// var question6 = prompt("What is your answer?");
+// var question7 = prompt("What is your answer?");
+// var question8 = prompt("What is your answer?");
+// var question9 = prompt("What is your answer?");
+// var question10 = prompt("What is your answer?");
+// var question11 = prompt("What is your answer?");
+// var question12 = prompt("What is your answer?");
+// var question13 = prompt("What is your answer?");
+// var question14 = prompt("What is your answer?");
+// var question15 = prompt("What is your answer?");
+// var question16 = prompt("What is your answer?");
+// var question17 = prompt("What is your answer?");
+// var question18 = prompt("What is your answer?");
+// var question19 = prompt("What is your answer?");
+// var question20 = prompt("What is your answer?");
